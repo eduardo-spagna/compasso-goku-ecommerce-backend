ee018bdc7a055d5f1b385c5ba662228bcb2ccc6c
Eduardo Spagna <eduardo@gempe.dev>
2021-03-02 21:29:42 -0300

    First commit

---
+++ src/main/java/com/compassouol/gokuecommerce/GokuEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.compassouol.gokuecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuEcommerceApplication.class, args);
+	}
+
+}
