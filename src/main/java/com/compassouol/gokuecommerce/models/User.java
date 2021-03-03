66fe10e7090af627bc3d8fc727da76086ea336e3
Eduardo Spagna <eduardo@gempe.dev>
2021-03-02 23:25:40 -0300

    Add User model

---
+++ src/main/java/com/compassouol/gokuecommerce/models/User.java
@@ -0,0 +1,41 @@
+package com.compassouol.gokuecommerce.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_created_at", updatable = false)
+    private LocalDateTime userCreatedAt = LocalDateTime.now();
+}
