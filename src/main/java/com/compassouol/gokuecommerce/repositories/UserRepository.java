2c864479a64d052c1596091f785ef49b7078b808
Eduardo Spagna <eduardo@gempe.dev>
2021-03-05 01:09:28 -0300

    Add method with native query to find user by email

--- src/main/java/com/compassouol/gokuecommerce/repositories/UserRepository.java
+++ src/main/java/com/compassouol/gokuecommerce/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.compassouol.gokuecommerce.repositories;
 import com.compassouol.gokuecommerce.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    @Query(value = "SELECT * FROM users WHERE user_email = :userEmail", nativeQuery = true)
+    User findUserByEmail(String userEmail);
 }
