a5545908be7b4530c4b8cd7f9897dbe2f49bb63c
Eduardo Spagna <eduardo@gempe.dev>
2021-03-05 00:50:00 -0300

    Add RoleRepository

---
+++ src/main/java/com/compassouol/gokuecommerce/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.compassouol.gokuecommerce.repositories;
+
+import com.compassouol.gokuecommerce.models.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    
+}
