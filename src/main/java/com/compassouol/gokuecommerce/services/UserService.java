f3435e89387df6403329a021e1ccd18d6f6e22ea
Eduardo Spagna <eduardo@gempe.dev>
2021-03-05 01:10:43 -0300

    Change user creation and add search methods by id and email

--- src/main/java/com/compassouol/gokuecommerce/services/UserService.java
+++ src/main/java/com/compassouol/gokuecommerce/services/UserService.java
@@ -1,6 +1,14 @@
 package com.compassouol.gokuecommerce.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import com.compassouol.gokuecommerce.dtos.request.user.CreateUserRequestDTO;
+import com.compassouol.gokuecommerce.enums.RoleEnum;
+import com.compassouol.gokuecommerce.models.Role;
 import com.compassouol.gokuecommerce.models.User;
 import com.compassouol.gokuecommerce.repositories.UserRepository;
 import com.compassouol.gokuecommerce.utils.GenerateHashPasswordUtil;
@@ -13,6 +21,27 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private UserRoleService userRoleService;
+
+    public User findUserById(long userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent() == true) {
+            return user.get();
+        }
+
+        return null;
+    }
+
+    public User findUserByEmail(String userEmail) {
+        return userRepository.findUserByEmail(userEmail);
+    }
+
+    @Transactional
     public User create(CreateUserRequestDTO createUser) {
         User user = new User();
 
@@ -20,6 +49,12 @@ public class UserService {
         user.setUserEmail(createUser.getUserEmail());
         user.setUserPassword(GenerateHashPasswordUtil.generateHash(createUser.getUserPassword()));
 
-        return userRepository.save(user);
+        User createdUser = userRepository.save(user);
+
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleService.findRoleById(RoleEnum.ROLE_ADMIN.getRoleId()));
+        userRoleService.createUserRole(createdUser, roles);
+
+        return createdUser;
     }
 }
